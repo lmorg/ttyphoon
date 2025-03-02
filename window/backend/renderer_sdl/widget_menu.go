7bd90ae6d1706f1b87b774b505e979c8f5048a50
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-03-02 12:15:46 +0000

    sdl: menu cropping should be win size not pane size

--- window/backend/renderer_sdl/widget_menu.go
+++ window/backend/renderer_sdl/widget_menu.go
@@ -92,7 +92,7 @@ func (sr *sdlRender) DisplayMenu(title string, options []string, highlightCallba
 		highlightIndex:    _MENU_HIGHLIGHT_INIT,
 	}
 
-	var crop = int(sr.termWin.Active.Term.GetSize().X - 20)
+	var crop = int(sr.winCellSize.X - 20)
 
 	for i := range sr.menu.options {
 		sr.menu.options[i] = runewidth.Truncate(sr.menu.options[i], int(crop), "…")
