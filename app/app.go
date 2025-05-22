185f08aa0ee9ea3a3400823c0a6585c758d39320
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-05-22 21:46:09 +0100

    chore: version number

--- app/app.go
+++ app/app.go
@@ -14,8 +14,8 @@ const Name = "Ttyphoon"
 const (
 	version  = "%d.%d.%d"
 	Major    = 0
-	Minor    = 10
-	Revision = 5200
+	Minor    = 11
+	Revision = 1000
 )
 
 const Title = "Ttyphoon - Multimedia Terminal Emulator"
