39fd947afadf5f549c8fd710211382a221dccb23
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-05-22 22:26:54 +0100

    ai: read `mcp.json` from $HOME

--- ai/start_mcp.go
+++ ai/start_mcp.go
@@ -1,13 +1,27 @@
 package ai
 
 import (
+	"os"
+
 	"github.com/lmorg/mxtty/ai/mcp_config"
 	_ "github.com/lmorg/mxtty/ai/tools"
+	"github.com/lmorg/mxtty/debug"
 )
 
 func init() {
 	err := mcp_config.StartServersFromJson("mcp.json")
 	if err != nil {
-		panic(err)
+		debug.Log(err)
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		debug.Log(err)
+		return
+	}
+
+	err = mcp_config.StartServersFromJson(home + "/mcp.json")
+	if err != nil {
+		debug.Log(err)
 	}
 }
