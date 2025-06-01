a7845696fe477c437b9ced96243c942e91330d1d
Laurence Morgan <mail@laurencemorgan.co.uk>
2025-06-01 12:43:14 +0100

    typeface: improved runewidth detection

--- utils/runewidth/runewidth.go
+++ utils/runewidth/runewidth.go
@@ -1,16 +1,23 @@
 package runewidth
 
 import (
+	"github.com/forPelevin/gomoji"
 	mattyn_runewidth "github.com/mattn/go-runewidth"
 	"github.com/rivo/uniseg"
 )
 
 func RuneWidth(r rune) int {
-	return uniseg.StringWidth(string(r))
+	if mattyn_runewidth.RuneWidth(r) == 2 ||
+		uniseg.StringWidth(string(r)) == 2 ||
+		gomoji.ContainsEmoji(string(r)) {
+		return 2
+	}
+
+	return 1
 }
 
 func StringWidth(s string) int {
-	return uniseg.StringWidth(s)
+	return max(mattyn_runewidth.StringWidth(s), uniseg.StringWidth(s))
 }
 
 func Truncate(s string, crop int, terminator string) string {
